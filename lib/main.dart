d5d3976160f7d9a50d24b7a3b33b63ddb630d972
Taleb Rayane <rayanetaleb170@gmail.com>
2026-03-06 17:05:33 +0100

    Mettre à jour tous les fichiers du projet

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'features/home/presentation/pages/home_page.dart';
+import 'features/reservasion/presentation/pages/reservasion_page.dart';
+import 'features/historique/presentation/pages/historique_page.dart';
+import 'features/profil/presentation/pages/profil_page.dart';
+import 'features/parametre/presentation/pages/parametre_page.dart';
+import 'features/auth/presentation/pages/login_page.dart'; // si tu as login
 
+import 'features/home/presentation/pages/student_main_page.dart'; // on crée cette page
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +17,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      title: 'UniMove',
+      theme: ThemeData(
+        primaryColor: const Color(0xFF3D3C3B),
+        scaffoldBackgroundColor: Colors.grey.shade900,
+      ),
+      home: const StudentMainPage(), // ← ici on met la page principale avec la BottomNavigationBar
     );
   }
 }
