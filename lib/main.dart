a21444f55ae4fbd4befea1c848f5e0a54bff109d
Taleb Rayane <rayanetaleb170@gmail.com>
2026-03-07 16:29:56 +0100

    mettre a jour toutes les pags modifiees

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,5 @@
 import 'package:flutter/material.dart';
-import 'features/auth/presentation/pages/login_page.dart'; 
-import 'features/home/presentation/pages/student_main_page.dart';
-import 'features/home/presentation/pages/home_page.dart';
-import 'features/reservation/presentation/pages/reservation_page.dart';
-import 'features/historique/presentation/pages/historique_page.dart';
-import 'features/profil/presentation/pages/profile_page.dart';
-import 'features/parametres/presentation/pages/parametres_page.dart';
-
- 
+import 'features/auth/presentation/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,11 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'UniMove',
-      theme: ThemeData(
-        primaryColor: const Color(0xFF3D3C3B),
-        scaffoldBackgroundColor: Colors.grey.shade900,
-      ),
-      home: const StudentMainPage(), 
+      theme: ThemeData.dark(), 
+      home: const LoginPage(),
     );
   }
 }
